855b511ac63241657647928180945b9a9a74ecc9
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-01-29 19:46:22 +0200

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react'
 import NavigationBar from './nav';
+import Welcome from './Welcome';
 const App = () => {
   return (
     <div>
    <NavigationBar/>
+   <Welcome/>
     </div>
   )
 }
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Welcome = () => {
   return (
     <div>
-      <h2 className='text'> Welcome to Job seekers Community</h2>
+      <h2 className='text text-center mt-64 my-4 text-5xl text-cyan-400 '> Welcome to Job seekers Community</h2>
     </div>
   )
 }
