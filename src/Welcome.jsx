5a1b52e0b3146fae7a96cec565aa1ab111bf8a36
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-01-30 15:31:47 +0200

    react

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Welcome = () => {
   return (
     <div>
+      <img src="{}" alt="" />
       <h2 className='text text-center mt-64 my-4 text-5xl text-red-600 outline-inherit'> Welcome to Job seekers Community</h2>
     </div>
   )
